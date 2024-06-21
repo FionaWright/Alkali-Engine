6ee440f8f8dcbe83ab5925a06b8a4f4b6f9a0adc
Fiona Wright <fiona.wgt@gmail.com>
2024-06-21 19:18:14 +0100

    Made wireframe/disable backcull modes not require restarting scene

--- Alkali Engine/SceneBistro.cpp
+++ Alkali Engine/SceneBistro.cpp
@@ -50,7 +50,7 @@ bool SceneBistro::LoadContent()
 
 	if (!ResourceTracker::TryGetShader("PBR.vs - PBR.ps", m_shaderPBR))
 	{
-		D3D12_INPUT_ELEMENT_DESC inputLayout[] =
+		vector<D3D12_INPUT_ELEMENT_DESC> inputLayout =
 		{
 			{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
 			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
@@ -59,7 +59,7 @@ bool SceneBistro::LoadContent()
 			{ "BINORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
 		};
 
-		m_shaderPBR->Init(L"PBR.vs", L"PBR.ps", inputLayout, _countof(inputLayout), rootSig.Get(), m_d3dClass->GetDevice());
+		m_shaderPBR->Init(L"PBR.vs", L"PBR.ps", inputLayout, rootSig.Get(), m_d3dClass->GetDevice());
 		//m_shaderCube->InitPreCompiled(L"Test_VS.cso", L"Test_PS.cso", inputLayout, _countof(inputLayout), rootSig);
 	}
 
--- Alkali Engine/Shader.h
+++ Alkali Engine/Shader.h
@@ -5,8 +5,24 @@
 
 using std::wstring_view;
 using std::wstring;
+using std::vector;
 
 const wstring g_dirPath = L"Assets/Shaders/";
+
+// For full list of fields (Order matters!)
+	// https://learn.microsoft.com/en-us/windows/win32/api/d3d12/ne-d3d12-d3d12_pipeline_state_subobject_type
+struct PipelineStateStream
+{
+	CD3DX12_PIPELINE_STATE_STREAM_ROOT_SIGNATURE pRootSignature;
+	CD3DX12_PIPELINE_STATE_STREAM_INPUT_LAYOUT InputLayout;
+	CD3DX12_PIPELINE_STATE_STREAM_PRIMITIVE_TOPOLOGY PrimitiveTopologyType;
+	CD3DX12_PIPELINE_STATE_STREAM_VS VS;
+	CD3DX12_PIPELINE_STATE_STREAM_PS PS;
+	CD3DX12_PIPELINE_STATE_STREAM_BLEND_DESC Blend;
+	CD3DX12_PIPELINE_STATE_STREAM_RASTERIZER RasterizerState;
+	CD3DX12_PIPELINE_STATE_STREAM_DEPTH_STENCIL_FORMAT DSVFormat;
+	CD3DX12_PIPELINE_STATE_STREAM_RENDER_TARGET_FORMATS RTVFormats;
+};
 
 class Shader
 {
@@ -14,10 +30,11 @@ public:
 	Shader();
 	~Shader();
 
-	void Init(const wstring& vsName, const wstring& psName, D3D12_INPUT_ELEMENT_DESC* inputLayout, UINT inputLayoutCount, ID3D12RootSignature* rootSig, ID3D12Device2* device, D3D12_PRIMITIVE_TOPOLOGY_TYPE topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
-	void InitPreCompiled(const wstring& vsName, const wstring& psName, D3D12_INPUT_ELEMENT_DESC* inputLayout, UINT inputLayoutCount, ID3D12RootSignature* rootSig, ID3D12Device2* device, D3D12_PRIMITIVE_TOPOLOGY_TYPE topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
+	void Init(const wstring& vsName, const wstring& psName, vector<D3D12_INPUT_ELEMENT_DESC> inputLayout, ID3D12RootSignature* rootSig, ID3D12Device2* device, D3D12_PRIMITIVE_TOPOLOGY_TYPE topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
+	void InitPreCompiled(const wstring& vsName, const wstring& psName, vector<D3D12_INPUT_ELEMENT_DESC> inputLayout, ID3D12RootSignature* rootSig, ID3D12Device2* device, D3D12_PRIMITIVE_TOPOLOGY_TYPE topology = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
 
 	void Compile(ID3D12Device2* device, ID3D12RootSignature* rootSig);
+	void UpdateRasterizerDesc(ID3D12Device2* device);
 
 	ComPtr<ID3D12PipelineState> GetPSO();
 
@@ -34,10 +51,11 @@ protected:
 	ComPtr<ID3DBlob> ReadPreCompiledShader(LPCWSTR path);
 
 private:
-	D3D12_INPUT_ELEMENT_DESC* m_inputLayout = nullptr;
-	UINT m_inputLayoutCount = 0;
+	vector<D3D12_INPUT_ELEMENT_DESC> m_inputLayout;
 	D3D12_PRIMITIVE_TOPOLOGY_TYPE m_topology;
 
+	ID3D12RootSignature* m_rootSig;
+	PipelineStateStream m_psoStream; // Remove
 	ComPtr<ID3D12PipelineState> m_pso;
 
 	bool m_preCompiled = false;
