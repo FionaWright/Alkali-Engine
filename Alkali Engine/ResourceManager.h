f585fc0309902ec96c9a3eca0b714cd376426796
Finn W <finn.wright5@gmail.com>
2024-07-29 13:59:40 +0100

    Minor changes

--- Alkali Engine/ResourceManager.h
+++ Alkali Engine/ResourceManager.h
@@ -5,7 +5,7 @@
 class ResourceManager
 {
 public:
-	static void CreateCommittedResource(ComPtr<ID3D12Resource>& pDestinationResource, size_t numElements, size_t elementSize, D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+	static void CreateCommittedResourceAsCommon(ComPtr<ID3D12Resource>& pDestinationResource, size_t numElements, size_t elementSize, D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
 
 	static void UploadCommittedResource(ID3D12GraphicsCommandList2* commandList, ComPtr<ID3D12Resource>& pDestinationResource, ID3D12Resource** pIntermediateResource, size_t numElements, size_t elementSize, const void* bufferData);
 
