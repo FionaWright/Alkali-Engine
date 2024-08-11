c0f5007552d670882457ffc53260d0880c04b96c
Finn W <finn.wright5@gmail.com>
2024-08-11 17:17:49 +0100

    Minor changes

--- Alkali Engine/Assets/Shaders/Skybox_PS.hlsl
+++ Alkali Engine/Assets/Shaders/Skybox_PS.hlsl
@@ -9,6 +9,6 @@ SamplerState g_sampler : register(s0);
 
 float4 main(V_OUT input) : SV_Target
 {
-    float3 col = g_cubemap.SampleLevel(g_sampler, input.ViewDirection, 1).rgb;
+    float3 col = g_cubemap.SampleLevel(g_sampler, input.ViewDirection, 0).rgb;
     return float4(col, 1);
 }
